b423bd4ee1823eb2dd29600411e007dcc5939439
collapsew <uauadadaya@mail.ru>
2023-02-17 19:07:19 +0300

    vol.1

--- weather.py
+++ weather.py
@@ -9,9 +9,9 @@ def weather(query):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41'})
     try:
         temp = r.html.find('span#wob_tm', first=True).text
-        unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
-        desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
-        print(f'The weather is now "{temp, unit, desc}"')
+        unit = r.html.find('div.vk_bk,wob-unit span.wob_t', first=True).text
+        desc = r.html.find('div.VQF4g', first=True).find('span#wob-dc', first=True).text
+        print(f'The weather is now "{temp}\n{unit}\n{desc}"')
     except:
         print(f'The city "{query}" was not found, try again"')
 
